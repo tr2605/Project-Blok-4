6e76a6f5fcecc8631e64df77f9286548a37305b7
Tim Rogier <t.rogier@me.com>
2023-06-16 08:57:01 +0200

    java

---
+++ site/script.js
@@ -0,0 +1,9 @@
+var outputElement = document.getElementById("output");
+
+// Function to display the text on the screen
+function displayText() {
+  outputElement.textContent = "adressen database";
+}
+
+// Call the displayText function when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", displayText);
